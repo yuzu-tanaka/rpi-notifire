e60db8a71db2a89bd7d128536af9f67801f2678f
yuzu tanaka <yuzutanaka@gmail.com>
2018-09-17 13:34:06 +0900

    入退店の基本動作完成

--- app.py
+++ app.py
@@ -3,17 +3,40 @@
 
 # server.py
 from flask import Flask, render_template, request
+import datetime
+
+# mongo db 
+from pymongo import MongoClient
+client = MongoClient('localhost', 27017) #接続先サーバ
+db = client['testDatabase'] #データベース
+collection = db['testCollection'] #コレクション
+
 
 app = Flask(__name__)
 
+def mongoPost(action): # 引数でアクションを渡して、それでmongoに書き込む
+## ここに店員進入の通知を書き込む
+    post = {"action": action,
+    "tags": ["とりあえず", "なんか進入", "データ登録"],
+    "date": datetime.datetime.utcnow()}
+    try:
+      result = collection.insert_one(post)
+      print(post)
+    except:
+      print(result)
+    return result
+
 # mongo-dbへの通知処理を行うところ
 def notify(entryLeave):
+
     if entryLeave == 'entry':
         #進入時の通知処理
-        ret = 'Entry'
+        ret = mongoPost(entryLeave)
+        ret = 'leave'
     elif entryLeave == 'leave':
         #退出時の通知処理
-        ret = 'leave'
+        ret = mongoPost(entryLeave)
+        ret = 'entry'
     else:
         ret = None
 
@@ -30,14 +53,14 @@ def entry():
     #val = request.args.get("msg", "Not defined")
     # ここに通知する処理を記述する
     ret = notify('entry')
-    return ret
+    return render_template('entry.html', message=u'退出',action=ret)
 
 @app.route('/leave')
 def hello():
     # request.argsにクエリパラメータが含まれている
     #val = request.args.get("msg", "Not defined")
     ret = notify('leave')
-    return ret
+    return render_template('entry.html', message=u'入店',action=ret)
 
 if __name__ == '__main__':
     app.debug = True
---
+++ static/app.js
@@ -0,0 +1,72 @@
+// 要素ら
+
+var $document = $(document);
+var $hitarea = $('#hitarea');
+var $eventname = $('#eventname');
+var $x = $('#x');
+var $y = $('#y');
+var action = document.getElementById("action");
+//window.alert(element.textContent);
+
+// タッチイベントが利用可能かの判別
+
+var supportTouch = 'ontouchend' in document;
+
+// イベント名
+
+var EVENTNAME_TOUCHSTART = supportTouch ? 'touchstart' : 'mousedown';
+var EVENTNAME_TOUCHMOVE = supportTouch ? 'touchmove' : 'mousemove';
+var EVENTNAME_TOUCHEND = supportTouch ? 'touchend' : 'mouseup';
+
+// 表示をアップデートする関数群
+
+var updateXY = function(event) {
+  // jQueryのイベントはオリジナルのイベントをラップしたもの。
+  // changedTouchesが欲しいので、オリジナルのイベントオブジェクトを取得
+  var original = event.originalEvent;
+  var x, y;
+  if(original.changedTouches) {
+    x = original.changedTouches[0].pageX;
+    y = original.changedTouches[0].pageY;
+  } else {
+    x = event.pageX;
+    y = event.pageY;
+  }
+  $x.text(x);
+  $y.text(y);
+};
+var updateEventname = function(eventname) {
+  $eventname.text(eventname);
+};
+
+// イベント設定
+
+var handleStart = function(event) {
+  // updateEventname(EVENTNAME_TOUCHSTART);
+  // updateXY(event);
+  $hitarea.css('background-color', 'red');
+  bindMoveAndEnd();
+};
+var handleMove = function(event) {
+  event.preventDefault(); // タッチによる画面スクロールを止める
+  // updateEventname(EVENTNAME_TOUCHMOVE);
+  // updateXY(event);
+};
+var handleEnd = function(event) {
+  // updateEventname(EVENTNAME_TOUCHEND);
+  // updateXY(event);
+  $hitarea.css('background-color', 'blue');
+  unbindMoveAndEnd();
+  // window.alert(action.textContent);
+  window.location.href = '/' + action.textContent;
+};
+var bindMoveAndEnd = function() {
+  $document.on(EVENTNAME_TOUCHMOVE, handleMove);
+  $document.on(EVENTNAME_TOUCHEND, handleEnd);
+};
+var unbindMoveAndEnd = function() {
+  $document.off(EVENTNAME_TOUCHMOVE, handleMove);
+  $document.off(EVENTNAME_TOUCHEND, handleEnd);
+};
+
+$hitarea.on(EVENTNAME_TOUCHSTART, handleStart);
